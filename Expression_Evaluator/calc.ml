31328d07984e28fad1a0197414835beadebe8413
ozym4nd145 <ozym4nd145@outlook.com>
2017-03-15 19:36:54 +0530

    Made printing of tree before substitution

--- Expression_Evaluator/calc.ml
+++ Expression_Evaluator/calc.ml
@@ -6,8 +6,9 @@ let _ =
     let lexbuf = Lexing.from_channel stdin in
     while true do
         let result = Parser.main Lexer.translate lexbuf in
+        let () = Printf.printf "%s\n" (string_of_parse_tree result) in
         let hash_map = make_kai [] result in
-        Printf.printf "%s\n%s\n" (string_of_parse_tree result) (string_of_mix ( eval_parse_tree hash_map result));
+        Printf.printf "Result => %s\n" (string_of_mix ( eval_parse_tree hash_map result));
         flush stdout
     done
     with Lexer.Eof ->
