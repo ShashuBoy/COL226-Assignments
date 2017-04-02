c0c0aa31d490035cd570919972fac8e31f977b8f
Suyash Agrawal <ozym4nd145@outlook.com>
2017-04-02 18:28:03 +0530

    Made vars efficient

--- Substitution_Unification/assignment4.ml
+++ Substitution_Unification/assignment4.ml
@@ -1,12 +1,8 @@
 exception NOT_UNIFIABLE;;
+module SS = Set.Make(String);;
 type variable = Var of string;;
 type symbol = Sym of string;;
 type term = V of variable | Node of symbol*(term list);;
-
-let rec search_sig elem = function
-    [] -> (false,(Sym(""),-1))
-|   (sym,arity)::xs -> if sym = elem then (true,(sym,arity)) else (search_sig elem xs)
-;;
 
 let rec _check_sig sym_list = function
     [] -> true
@@ -16,10 +12,8 @@ let check_sig signature = _check_sig [] signature;;
 
 let rec wfterm signature = function
     V(x) -> true
-|   Node(s,lst) -> try 
-                    let (_,arity) = List.find (fun (x,arity) -> x = s) signature in if (List.length lst) = arity then (List.for_all (wfterm signature) lst) else false
-                   with
-                    Not_found -> false
+|   Node(s,lst) -> try let (_,arity) = List.find (fun (x,arity) -> x = s) signature in if (List.length lst) = arity then (List.for_all (wfterm signature) lst) else false
+                   with Not_found -> false
 ;;
 
 let rec ht = function
@@ -33,11 +27,11 @@ let rec sz acc_size = function
 ;;
 let size x = sz 0 x;;
 
-let rec _vars lst = function
-    V(x) -> if (List.exists ((fun b a -> b = a) x) lst) then lst else (x::lst)
-|   Node(sym,args) -> List.fold_left (fun a b -> _vars a b) lst args
+let rec _vars set = function
+    V(Var(x)) -> SS.add x set
+|   Node(sym,args) -> List.fold_left (fun a b -> _vars a b) set args
 ;;
-let vars term = _vars [] term;;
+let vars term = _vars SS.empty term;;
 
 let rec subst sigma = function
     V(x) -> sigma x
@@ -53,8 +47,9 @@ let rec mgu t1 t2 = match (t1,t2) with
     (Node(x,lst1),Node(y,lst2)) -> if (x <> y) || ((List.length lst1) <> (List.length lst2)) then raise NOT_UNIFIABLE
                                    else List.fold_left2 (fun s a b -> compose (mgu (subst s a) (subst s b)) s ) idty_subst lst1 lst2
 |   (V(x),V(y)) -> fun var -> if var = x then (V y) else (V var)
-|   (V(x),Node(f,lst)) -> if (List.exists ((fun b a -> b = a) x) (vars (Node(f,lst))) ) then raise NOT_UNIFIABLE else (fun var -> if var = x then (Node(f,lst)) else (V var) )
+|   (V(x),Node(f,lst)) -> if SS.exists (fun a -> Var(a)=x) (vars (Node (f,lst))) then raise NOT_UNIFIABLE else (fun var -> if var = x then (Node(f,lst)) else (V var) )
 |   (Node(f,lst),V(x)) -> mgu (V x) (Node (f,lst))
 ;;
 
-let print_subst t1 t2 subst = let var1 = _vars [] t1 in let var2 = _vars var1 t2 in List.map (fun x -> (x,(subst x))) var2;; 
+let print_subst t1 t2 subst = let var1 = vars t1 in let var2 = _vars var1 t2 in SS.fold (fun x lst -> (Var(x),(subst (Var x)))::lst ) var2 [];; 
+let print_vars t = let v = vars t in SS.fold (fun x lst -> (Var x)::lst ) v [];;
